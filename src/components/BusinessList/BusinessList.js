0e510cdc3e7915da9d93c8a9d3299cac4664060a
Max Sealey <maxsealey@users.noreply.github.com>
2022-03-03 17:04:35 -0700

    Integrated Yelp API

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,8 +5,8 @@ import Business from '../Business/Business.js';
 class BusinessList extends React.Component {
     render() {
         return (
-            <div className = "BusinessList" key={business.id}>
-                { this.props.businesses.map(business => <Business business={business}/>) }
+            <div className = "BusinessList">
+                { this.props.businesses.map(business => <Business business={business} key={business.id}/>) }
             </div>
         );
     }
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -13,7 +13,7 @@ const Yelp = {
                 return jsonResponse.businesses.map((business)=> {
                     return {
                         id:business.id,
-                        imageSrc:business.photos[0],
+                        imageSrc:business.image_url,
                         name:business.name,
                         address:business.location.address1,
                         city:business.location.city,
