1085fd106ed7b8d9935336c6e36e247b09aa8b1e
Max Sealey <maxsealey@users.noreply.github.com>
2022-05-10 16:46:53 -0700

    minor change

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,5 +1,3 @@
-const apiKey = 'i0oe30_lOLnmAJlfPELkuYiOIpkjRtZbFAXo1fO9q924UpjbgETgDW9k6A26m867SQtuSRsTC3Ryuzx5SXkY-bxQzS3EwHkyag6MUP3-L7MSCiDC7FnFYY6IZyogYnYx'; // Insert API key here.
-
 const Yelp = {
   search(term, location, sortBy) {
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
